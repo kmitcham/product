42dba039d038b484d3d71145196669393b5d5e80
kmitcham <kmitcham@gmail.com>
2021-02-06 21:48:47 -0800

    fix an errant test

--- src/test/java/com/kevinmitcham/product/field/DescriptionTests.java
+++ src/test/java/com/kevinmitcham/product/field/DescriptionTests.java
@@ -30,8 +30,12 @@ public class DescriptionTests {
     }
     @Test
     public void testLongDescription(){
-        String inputLine = "80000001_01234567890123456789012345678901234567890123456789012345678900000567_00000000 00000000 00000000 00000000 00000000 NNNNNNNNN      18oz";
-        String expected =           "0123456789012345678901234567890123456789012345678901234567";
+    //   123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+    //            1         2         3         4         5         6         7         8         9         0         1         2         3         4         5
+        String inputLine = 
+        "80000001_01234567890123456789012345678901234567890123456789012345678_00000567 00000000 00000000 00000000 00000000 00000000 NNNNNNNNN      18oz";
+        String expected =           
+                 "01234567890123456789012345678901234567890123456789012345678";
         description.addFieldToProduct(inputLine, productRecord);
         String actual = productRecord.getDescription();
         assertEquals(expected, actual, "long description works");
